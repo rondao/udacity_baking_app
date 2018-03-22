c97216f881b8312ae30c6d7de4b87ca03147cc92
Rafael Rondão <rafael.rondao@gmail.com>
2018-03-22 18:49:30 -0300

    Creating Activity to show recipes with a RecyclerView.
    Signed-off-by: Rafael Rondão <rafael.rondao@gmail.com>

---
+++ app/src/main/java/com/rondao/ubakingapp/view/RecipesActivity.java
@@ -0,0 +1,15 @@
+package com.rondao.ubakingapp.view;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.rondao.ubakingapp.R;
+
+public class RecipesActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_recipes);
+    }
+}
