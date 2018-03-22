b60ab63036100f3d5588cb2c6031b584ca4f30c5
Rafael Rondão <rafael.rondao@gmail.com>
2018-03-22 19:12:28 -0300

    Creating Ingredient, Recipe and RecipeStep POJOs as models.
    Signed-off-by: Rafael Rondão <rafael.rondao@gmail.com>

---
+++ app/src/main/java/com/rondao/ubakingapp/model/Ingredient.java
@@ -0,0 +1,32 @@
+package com.rondao.ubakingapp.model;
+
+public class Ingredient {
+    private float quantity;
+    private String measure;
+    //@SerializedName("ingredient")
+    private String name;
+
+    public float getQuantity() {
+        return quantity;
+    }
+
+    public void setQuantity(float quantity) {
+        this.quantity = quantity;
+    }
+
+    public String getMeasure() {
+        return measure;
+    }
+
+    public void setMeasure(String measure) {
+        this.measure = measure;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
---
+++ app/src/main/java/com/rondao/ubakingapp/model/Recipe.java
@@ -0,0 +1,60 @@
+package com.rondao.ubakingapp.model;
+
+import java.util.List;
+
+public class Recipe {
+    private int id;
+    private String name;
+    private List<Ingredient> ingredients;
+    private List<RecipeStep> steps;
+    private String servings;
+    private String image;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public List<Ingredient> getIngredients() {
+        return ingredients;
+    }
+
+    public void setIngredients(List<Ingredient> ingredients) {
+        this.ingredients = ingredients;
+    }
+
+    public List<RecipeStep> getSteps() {
+        return steps;
+    }
+
+    public void setSteps(List<RecipeStep> steps) {
+        this.steps = steps;
+    }
+
+    public String getServings() {
+        return servings;
+    }
+
+    public void setServings(String servings) {
+        this.servings = servings;
+    }
+
+    public String getImage() {
+        return image;
+    }
+
+    public void setImage(String image) {
+        this.image = image;
+    }
+}
