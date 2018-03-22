a3fe4e8d56e0bda1484ab2ada8362912eb5416a5
Rafael Rondão <rafael.rondao@gmail.com>
2018-03-22 19:35:43 -0300

    Creating RetroFit ApiInterface to get and parse remote json.
    Signed-off-by: Rafael Rondão <rafael.rondao@gmail.com>

---
+++ app/src/main/java/com/rondao/ubakingapp/data/BakingApi.java
@@ -0,0 +1,30 @@
+package com.rondao.ubakingapp.data;
+
+import com.rondao.ubakingapp.model.Recipe;
+
+import java.util.List;
+
+import retrofit2.Call;
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
+import retrofit2.http.GET;
+
+public class BakingApi {
+    public static final String BASE_URL = "https://d17h27t6h515a5.cloudfront.net/topher/2017/May/59121517_baking/";
+    private static Retrofit retrofit = null;
+
+    public static Retrofit getClient() {
+        if (retrofit==null) {
+            retrofit = new Retrofit.Builder()
+                    .baseUrl(BASE_URL)
+                    .addConverterFactory(GsonConverterFactory.create())
+                    .build();
+        }
+        return retrofit;
+    }
+
+    public interface ApiInterface {
+        @GET("baking.json")
+        Call<List<Recipe>> getBakingRecipes();
+    }
+}
