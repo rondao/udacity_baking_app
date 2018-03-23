d1ea5c23a51a968a1c8b8629024276f67117ad89
Rafael Rondão <rafael.rondao@gmail.com>
2018-03-23 12:19:23 -0300

    Moving RecipesActivity inside Recipe package.
    Align organization with resources folder.
    
    Signed-off-by: Rafael Rondão <rafael.rondao@gmail.com>

--- app/src/main/java/com/rondao/ubakingapp/view/recipes/RecipesActivity.java
+++ app/src/main/java/com/rondao/ubakingapp/view/recipes/RecipesActivity.java
@@ -1,9 +1,18 @@
-package com.rondao.ubakingapp.view;
+package com.rondao.ubakingapp.view.recipes;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 
 import com.rondao.ubakingapp.R;
+import com.rondao.ubakingapp.data.BakingApi;
+import com.rondao.ubakingapp.model.Recipe;
+
+import java.util.List;
+
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
 
 public class RecipesActivity extends AppCompatActivity {
 
