8baa9f9b119d92d81cfc5b86dce31d3712577ed8
Rafael Rondão <rafael.rondao@gmail.com>
2018-03-23 13:31:27 -0300

    Creating RecipeAdapter to show Recipes in RecyclerView.
    Signed-off-by: Rafael Rondão <rafael.rondao@gmail.com>

---
+++ app/src/main/java/com/rondao/ubakingapp/view/recipes/RecipeAdapter.java
@@ -0,0 +1,60 @@
+package com.rondao.ubakingapp.view.recipes;
+
+import android.content.Context;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import com.rondao.ubakingapp.R;
+import com.rondao.ubakingapp.model.Recipe;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class RecipeAdapter extends RecyclerView.Adapter<RecipeAdapter.RecipeViewHolder> {
+    private List<Recipe> mRecipeData;
+
+    public RecipeAdapter() {
+        mRecipeData = new ArrayList<>(0);
+    }
+
+    public class RecipeViewHolder extends RecyclerView.ViewHolder {
+        public final TextView mRecipeName;
+        public final TextView mRecipeServings;
+
+        public RecipeViewHolder(View itemView) {
+            super(itemView);
+            mRecipeName = (TextView) itemView.findViewById(R.id.tv_recipe_name);
+            mRecipeServings = (TextView) itemView.findViewById(R.id.tv_recipe_servings);
+        }
+    }
+
+    @Override
+    public RecipeViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        Context context = parent.getContext();
+        LayoutInflater inflater = LayoutInflater.from(context);
+
+        View view = inflater.inflate(R.layout.recipe_card, parent, false);
+        return new RecipeViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(RecipeViewHolder holder, int position) {
+        Recipe recipe = mRecipeData.get(position);
+
+        holder.mRecipeName.setText(recipe.getName());
+        holder.mRecipeServings.setText(recipe.getServings());
+    }
+
+    @Override
+    public int getItemCount() {
+        return mRecipeData.size();
+    }
+
+    public void setRecipeData(List<Recipe> mRecipeData) {
+        this.mRecipeData = mRecipeData;
+        notifyDataSetChanged();
+    }
+}
--- app/src/main/java/com/rondao/ubakingapp/view/recipes/RecipesActivity.java
+++ app/src/main/java/com/rondao/ubakingapp/view/recipes/RecipesActivity.java
@@ -2,12 +2,15 @@ package com.rondao.ubakingapp.view.recipes;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.GridLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.util.Log;
 
 import com.rondao.ubakingapp.R;
 import com.rondao.ubakingapp.data.BakingApi;
 import com.rondao.ubakingapp.model.Recipe;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import retrofit2.Call;
@@ -15,10 +18,20 @@ import retrofit2.Callback;
 import retrofit2.Response;
 
 public class RecipesActivity extends AppCompatActivity {
+    private RecipeAdapter mRecipeAdapter;
+    private RecyclerView mRecyclerView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_recipes);
+
+        mRecyclerView = (RecyclerView) findViewById(R.id.rv_recipes);
+
+        mRecyclerView.setLayoutManager(new GridLayoutManager(this, 1));
+        mRecyclerView.setHasFixedSize(true);
+
+        mRecipeAdapter = new RecipeAdapter();
+        mRecyclerView.setAdapter(mRecipeAdapter);
     }
 }
