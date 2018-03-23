eec80394330bc5a4ade77f352cd4c34cb054cb24
Rafael Rondão <rafael.rondao@gmail.com>
2018-03-23 17:40:43 -0300

    Checking if null for data set.
    Signed-off-by: Rafael Rondão <rafael.rondao@gmail.com>

--- app/src/main/java/com/rondao/ubakingapp/view/recipes/RecipeAdapter.java
+++ app/src/main/java/com/rondao/ubakingapp/view/recipes/RecipeAdapter.java
@@ -4,15 +4,10 @@ import com.rondao.ubakingapp.R;
 import com.rondao.ubakingapp.model.Recipe;
 import com.rondao.ubakingapp.util.GenericAdapter;
 
-import java.util.ArrayList;
 import java.util.List;
 
 public class RecipeAdapter extends GenericAdapter {
     private List<Recipe> mRecipeData;
-
-    public RecipeAdapter() {
-        mRecipeData = new ArrayList<>(0);
-    }
 
     public void setRecipeData(List<Recipe> mRecipeData) {
         this.mRecipeData = mRecipeData;
@@ -31,6 +26,6 @@ public class RecipeAdapter extends GenericAdapter {
 
     @Override
     public int getItemCount() {
-        return mRecipeData.size();
+        return mRecipeData == null ? 0 : mRecipeData.size();
     }
 }
