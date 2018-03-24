f92f86dff861b58cd78f550e3c40691f76761242
Rafael Rondão <rafael.rondao@gmail.com>
2018-03-24 19:25:12 -0300

    Creating empty fragment to show Recipe details.
    Signed-off-by: Rafael Rondão <rafael.rondao@gmail.com>

--- app/src/main/java/com/rondao/ubakingapp/activities/recipes/details/DetailsActivity.java
+++ app/src/main/java/com/rondao/ubakingapp/activities/recipes/details/DetailsActivity.java
@@ -4,20 +4,23 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 import com.rondao.ubakingapp.R;
-import com.rondao.ubakingapp.data.model.Recipe;
-
-import org.parceler.Parcels;
 
 public class DetailsActivity extends AppCompatActivity {
     public static final String EXTRA_RECIPE = "recipe";
 
-    private Recipe mRecipe;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_details);
 
-        mRecipe = Parcels.unwrap(getIntent().getParcelableExtra(EXTRA_RECIPE));
+        if (savedInstanceState == null) {
+            DetailsFragment detailsFragment = new DetailsFragment();
+            detailsFragment.setArguments(getIntent().getExtras());
+
+            getSupportFragmentManager().beginTransaction()
+                    .add(R.id.fragment_details, detailsFragment).commit();
+        }
     }
 }
---
+++ app/src/main/java/com/rondao/ubakingapp/activities/recipes/details/DetailsFragment.java
@@ -0,0 +1,24 @@
+package com.rondao.ubakingapp.activities.recipes.details;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.rondao.ubakingapp.R;
+
+public class DetailsFragment extends Fragment {
+
+    private View mView;
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        mView = inflater.inflate(R.layout.fragment_details, container, false);
+
+        return mView;
+    }
+
+}
