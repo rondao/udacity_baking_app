49eb0e611a3c182c5b326e0516b03b8b5e89b30c
Rafael Rondão <rafael.rondao@gmail.com>
2018-03-24 19:37:53 -0300

    Setting title with recipe name for DetailsActivity.
    Signed-off-by: Rafael Rondão <rafael.rondao@gmail.com>

--- app/src/main/java/com/rondao/ubakingapp/activities/recipes/details/DetailsActivity.java
+++ app/src/main/java/com/rondao/ubakingapp/activities/recipes/details/DetailsActivity.java
@@ -4,16 +4,20 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 import com.rondao.ubakingapp.R;
+import com.rondao.ubakingapp.data.model.Recipe;
+
+import org.parceler.Parcels;
 
 public class DetailsActivity extends AppCompatActivity {
     public static final String EXTRA_RECIPE = "recipe";
-
-
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_details);
+
+        Recipe recipe = Parcels.unwrap(getIntent().getParcelableExtra(EXTRA_RECIPE));
+        getSupportActionBar().setTitle(recipe.getName());
 
         if (savedInstanceState == null) {
             DetailsFragment detailsFragment = new DetailsFragment();
