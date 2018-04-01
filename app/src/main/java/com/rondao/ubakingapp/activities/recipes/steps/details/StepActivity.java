07e7277635be66baf0603a0b98a97939ffa16bb3
Rafael Rondão <rafael.rondao@gmail.com>
2018-04-01 16:09:02 -0300

    Fixing Issue when opening StepActivity while rotated.
    When rotated, fragments step_description_fragment and
    step_navigation_fragment won't exist.
    
    They only exist at portrait layout, not landscape.
    
    If StepActivity is started rotated, these Fragments
    can be binding directly to the FrameLayout ids.
    
    So they are all created not binded by Tag, and
    only binding when the FrameLayouts views exist.
    
    Signed-off-by: Rafael Rondão <rafael.rondao@gmail.com>

--- app/src/main/java/com/rondao/ubakingapp/activities/recipes/steps/details/StepActivity.java
+++ app/src/main/java/com/rondao/ubakingapp/activities/recipes/steps/details/StepActivity.java
@@ -2,6 +2,8 @@ package com.rondao.ubakingapp.activities.recipes.steps.details;
 
 import android.content.res.Configuration;
 import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
 
 import com.rondao.ubakingapp.R;
@@ -21,11 +23,16 @@ public class StepActivity extends AppCompatActivity implements StepNavigationFra
     public static final String EXTRA_STEP = "step";
     public static final String EXTRA_STEPS = "steps";
 
-    @State int mCurrentStep;
-    private List<RecipeStep> mListSteps;
+    public static final String TAG_VIDEO_FRAGMENT = "video";
+    public static final String TAG_DESCRIPTION_FRAGMENT = "description";
+    public static final String TAG_NAVIGATION_FRAGMENT = "navigation";
 
     private StepVideoFragment mStepVideoFragment;
     private StepDescriptionFragment mStepDescriptionFragment;
+    private StepNavigationFragment mStepNavigationFragment;
+
+    @State int mCurrentStep;
+    private List<RecipeStep> mListSteps;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -43,10 +50,9 @@ public class StepActivity extends AppCompatActivity implements StepNavigationFra
             RecipeStep recipe = Parcels.unwrap(getIntent().getParcelableExtra(EXTRA_STEP));
             mCurrentStep = mListSteps.indexOf(recipe);
 
-            initFragments();
-         } else {
-            retrieveExistingFragment();
-        }
+            createFragments();
+         }
+         initFragments();
     }
 
     @Override
@@ -61,30 +67,49 @@ public class StepActivity extends AppCompatActivity implements StepNavigationFra
         getSupportActionBar().hide();
     }
 
-    public void initFragments() {
+    public void createFragments() {
         mStepVideoFragment = new StepVideoFragment();
-        mStepDescriptionFragment = StepDescriptionFragment.newInstance(mListSteps.get(mCurrentStep));
-        StepNavigationFragment stepNavigationFragment = new StepNavigationFragment();
-
-        mStepVideoFragment.setArguments(getIntent().getExtras());
-        stepNavigationFragment.setArguments(getIntent().getExtras());
+        mStepDescriptionFragment = new StepDescriptionFragment();
+        mStepNavigationFragment = new StepNavigationFragment();
 
         getSupportFragmentManager().beginTransaction()
-                .add(R.id.step_video_fragment, mStepVideoFragment)
-                .add(R.id.step_description_fragment, mStepDescriptionFragment)
-                .add(R.id.step_navigation_fragment, stepNavigationFragment)
+                .add(mStepVideoFragment, TAG_VIDEO_FRAGMENT)
+                .add(mStepDescriptionFragment, TAG_DESCRIPTION_FRAGMENT)
+                .add(mStepNavigationFragment, TAG_NAVIGATION_FRAGMENT)
                 .commit();
     }
 
-    public void retrieveExistingFragment() {
-        mStepVideoFragment = (StepVideoFragment) getSupportFragmentManager().findFragmentById(R.id.step_video_fragment);
-        mStepDescriptionFragment  = (StepDescriptionFragment) getSupportFragmentManager().findFragmentById(R.id.step_description_fragment);
+    public void initFragments() {
+        mStepVideoFragment = getFragmentIfExists(TAG_VIDEO_FRAGMENT, mStepVideoFragment);
+        mStepDescriptionFragment = getFragmentIfExists(TAG_DESCRIPTION_FRAGMENT, mStepDescriptionFragment);
+        mStepNavigationFragment = getFragmentIfExists(TAG_NAVIGATION_FRAGMENT, mStepNavigationFragment);
 
         Bundle args = new Bundle();
         args.putParcelable(EXTRA_STEP, Parcels.wrap(mListSteps.get(mCurrentStep)));
 
         mStepVideoFragment.setArguments(args);
         mStepDescriptionFragment.setArguments(args);
+        mStepNavigationFragment.setArguments(args);
+
+        FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
+        if (findViewById(R.id.step_video_fragment) != null) {
+            transaction.replace(R.id.step_video_fragment, mStepVideoFragment);
+        }
+        if (findViewById(R.id.step_description_fragment) != null) {
+            transaction.replace(R.id.step_description_fragment, mStepDescriptionFragment);
+        }
+        if (findViewById(R.id.step_navigation_fragment) != null) {
+            transaction.replace(R.id.step_navigation_fragment, mStepNavigationFragment);
+        }
+        transaction.commit();
+    }
+
+    private <T extends Fragment> T getFragmentIfExists(String tag, T fragment) {
+        T savedFragment = (T) getSupportFragmentManager().findFragmentByTag(tag);
+        if (savedFragment != null) {
+            fragment = savedFragment;
+        }
+        return fragment;
     }
 
     @Override
